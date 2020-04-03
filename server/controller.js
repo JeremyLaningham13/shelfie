5bd068754628438ba9a338790f49e611606aea09
Jeremy Laningham <JeremyLaningham@gmail.com>
2020-04-03 17:21:09 -0500

    step 3 done

--- server/controller.js
+++ server/controller.js
@@ -1,6 +1,6 @@
 const getInventory = (req, res) => {
     const db = req.app.get('db')
-        db.getInventory().then(result=>{
+        db.get_Inventory().then(result=>{
             res.status(200).send(result)
         })
 }
